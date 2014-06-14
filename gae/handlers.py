4265154a4eada2aa00bf0e806f7ceec9bfb6540b
falko <falko@falko-ThinkPad>
2014-06-14 12:15:12 +0200

    send headers from the client to grooveshark

--- gae/handlers.py
+++ gae/handlers.py
@@ -1,7 +1,7 @@
 import webapp2
 
 url = 'http://grooveshark.com'
-html5_url = 'http://grooveshark.com'
+html5_url = 'http://html5.grooveshark.com'
 import urllib2
 
 
@@ -11,6 +11,16 @@ class MainHandler(webapp2.RequestHandler):
         #    return self.response.out.write('')
         self.response.headers['Access-Control-Allow-Origin'] = '*'
         self.response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        
+        #proxy = urllib2.ProxyHandler({'http': 'IP:PORT'})
+        #opener = urllib2.build_opener(proxy)
+        
+        opener = urllib2.build_opener()
+        urllib2.install_opener(opener)
+        
+        # submit the client headers, so that the request isn't denied
+        for h in ['User-Agent', 'Accept-Language', 'Accept']:
+            opener.addheaders = [(h, str(self.request.headers[h]))]
 
         conn = urllib2.urlopen(html5_url if self.request.get('html5') == "true" else url)
         html = conn.read()
